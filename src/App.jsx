f187b7e8c850d698e691f6fc893ee880854ef402
gradder7 <mehra3228@gmail.com>
2025-01-27 17:02:23 +0530

    added fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Services from "./Components/ServicesSection/Services";
 import FAQSection from "./Components/FAQSection/FAQSection";
 import BlogsExplore from "./Components/BlogsExplore/BlogsExplore";
 import Footer from "./Components/Footer/Footer";
+import Features from "./Components/Features/Features";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
       <Services />
       <FAQSection />
       <BlogsExplore />
+      <Features />
       <Footer />
     </div>
   );
